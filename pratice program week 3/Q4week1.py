e06f89a082c0869ab77417ee98a3ea7ffa5a7ae4
hemanthav04072008 <hemanthav04072008@gmail.com>
2026-09-19 11:24:42 +0530

    Remove Week 3 practice programs from session 2

--- pratice program week 3/Q4week1.py
+++
@@ -1,9 +0,0 @@
-number1 = int (input("Enter the first number"))
-number2 = int (input("Enter the second number"))
-a=number1
-b=number2
-while(number2 !=0):
-    reminder=number1%number2
-    number1=number2
-    number2=reminder
-print("The GCD of ",a,'and',b,'is',number1)
--- pratice program week 3/q2 week 1.py
+++
@@ -1,5 +0,0 @@
-a = int (input("Enter the number"))
-for i in range (0,1+a):
-    for j in range (0,i+1):
-        print ("*",end="")
-    print()
